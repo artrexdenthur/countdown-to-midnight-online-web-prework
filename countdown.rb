a32949011eff8adddb168108083d4736142fd09f
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 04:24:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,17 @@
 
 def countdown(int)
   while int > 0 
-    puts "#{int} SECOND" + int == 1? "!" : "S!"
+    puts "#{int} SECOND(S)!"
     int -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+end
+    
